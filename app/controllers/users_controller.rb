6d38f5656519f79351a69a5b8f73b49ebbad8d3c
ambapo <charlomuse@gmail.com>
2013-08-18 17:31:22 +0300

    website up

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,7 @@
 class UsersController < ApplicationController
 	before_filter :signed_in_user, only: [:index, :edit, :update]
+	before_filter :correct_user,   only: [:edit, :update]
+	before_filter :admin_user,     only: :destroy
 
 	def index
 		@users = User.paginate(page: params[:page])
@@ -33,4 +35,27 @@ class UsersController < ApplicationController
   def edit
   	@user = User.find(params[:id])
   end
+
+  def update
+  	if @user.update_attributes(params[:user])
+  		flash[:success] = "Profile updated"
+  		sing_in @user
+  		redirect_to @user
+  		#Handle a successful update.
+  	else
+  		render 'edit'
+  	end
+  	private 
+   		def signed_in_user
+   			redirect_to signin_url, notice: "Please sign in." unless signed_in?
+   		end
+
+   		def correct_user
+   			@user = User.find(params[:id])
+   			redirect_to(root_url) unless current_user?(@user)
+   		end
+
+   		def admin_user
+   			redirect_to(root_url) unless current_user.admin?
+   		end
   end
