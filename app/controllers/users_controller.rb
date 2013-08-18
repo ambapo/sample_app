00ddd15d5773bdd7f66366be7ea1be599b04e134
ambapo <charlomuse@gmail.com>
2013-08-18 15:17:31 +0300

    complete gemfile

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,36 @@
 class UsersController < ApplicationController
-  def new
+	before_filter :signed_in_user, only: [:index, :edit, :update]
+
+	def index
+		@users = User.paginate(page: params[:page])
+	end
+
+  def show
+    @user = User.find(params[:id])
   end
-end
+
+  def new
+  	@user= User.new
+  end
+
+  def create
+  	@user = User.new(params[:user])
+  	if @user.save
+  		flash[:success] = "Welcome to the Sample App"
+  		redirect_to @user
+  		#Handle a successful save
+  	else
+  		render 'new'
+  	end
+  end
+
+  def destroy
+  	User.find(params[:id]).destroy
+  	flash[:success] = "User destroyed."
+  	redirect_to users_url
+  end
+
+  def edit
+  	@user = User.find(params[:id])
+  end
+  end
